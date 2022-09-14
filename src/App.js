f47959673a9e93bb4c42d04b20cf43154463d16d
blacksquare573 <blacksquare573@gmail.com>
2022-09-14 09:08:01 +0900

    add search page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import RecruitInfoBackground from "./components/RecruitInfo/RecruitInfoBackground.component";
 import RegisterBackground from "./components/RegisterPage/RegisterBackground.component";
+import SearchPage from "./components/SearchPage/SearchPage.component";
 
 function App() {
   return (
     <div className="App">
-      <RecruitInfoBackground />
+      <SearchPage />
     </div>
   );
 }
